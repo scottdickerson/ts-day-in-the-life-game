3eb2e336420ea5d6ee40ce5901da5ac0b5f14a02
Scott Dickerson <6663002+scottdickerson@users.noreply.github.com>
2025-10-11 18:03:26 -0500

    chore(build): build with node

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
+import node from '@astrojs/node'
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,6 +12,8 @@ export default defineConfig({
             applyBaseStyles: false,
         }),
     ],
-    output: 'static',
-    // By default we're building a static site generation for building local html files that can be served with nginx however by copying the astro.config.vercel.serverless.mjs file to astro.config.mjs we can change the output to serverless.
+    output: 'server',
+    adapter: node({
+        mode: 'standalone',
+    }),
 })
