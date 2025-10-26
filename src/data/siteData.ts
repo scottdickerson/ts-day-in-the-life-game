8afe2d51199a761d1f46b937f9e4867a32c7b9c3
Scott Dickerson <6663002+scottdickerson@users.noreply.github.com>
2025-10-26 10:28:58 -0600

    fix(select): italicize dinosaurs names on select screen

--- src/data/siteData.ts
+++ src/data/siteData.ts
@@ -13,34 +13,34 @@ export const dinos: DinoOverview[] = [
         name: 'Agujaceratops',
         image: '/SelectedIndividualScreenAssets/SelectedIndividualScreenAssets-Aguja.webp',
         description:
-            'You are an adult Agujaceratops and you are currently without a herd. You will need to find food and choose how to interact with other animals all by yourself!',
+            'You are an adult <i>Agujaceratops</i> and you are currently without a herd. You will need to find food and choose how to interact with other animals all by yourself!',
     },
     {
         id: DinosaurTypeEnum.Krito,
         name: 'Kritosaurus',
         image: '/SelectedIndividualScreenAssets/SelectedIndividualScreenAssets-Krito.webp',
         description:
-            'You are a young Kritosaurus living in a herd. Even though you are not as big as some of the older adults in the herd, this is your first year looking for a mate.',
+            'You are a young <i>Kritosaurus</i> living in a herd. Even though you are not as big as some of the older adults in the herd, this is your first year looking for a mate.',
     },
     {
         id: DinosaurTypeEnum.Tyranno,
         name: 'Tyrannosaurus',
         image: '/SelectedIndividualScreenAssets/SelectedIndividualScreenAssets-Tyranno.webp',
         description:
-            'You are a parent Tyrannosaurus. Your nest is in a secluded spot within walking distance of the river. Your eggs will hatch soon, so you need to find food for yourself and your offspring',
+            'You are a parent <i>Tyrannosaurus</i>. Your nest is in a secluded spot within walking distance of the river. Your eggs will hatch soon, so you need to find food for yourself and your offspring',
     },
     {
         id: DinosaurTypeEnum.Mosa,
         name: 'Mosasaurus',
         image: '/SelectedIndividualScreenAssets/SelectedIndividualScreenAssets-Mosa.webp',
         description:
-            'You are a Mosasaurus whose territory is near a dormant undersea volcano. You need lots of food to sustain yourself, so you better get to it!',
+            'You are a <i>Mosasaurus</i> whose territory is near a dormant undersea volcano. You need lots of food to sustain yourself, so you better get to it!',
     },
     {
         id: DinosaurTypeEnum.Protos,
         name: 'Protostega',
         image: '/SelectedIndividualScreenAssets/SelectedIndividualScreenAssets-Proto.webp',
         description:
-            'You are a mature Protostega, measuring 8 feet long from nose to tail.',
+            'You are a mature <i>Protostega</i>, measuring 8 feet long from nose to tail.',
     },
 ]
